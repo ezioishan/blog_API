e3819a1b362d0db47db3dae1accc0ece672e92b8
YashMeh <mehrotra.yash9@gmail.com>
2019-05-07 23:53:55 +0530

    Added the ignore file

--- server.js
+++ server.js
@@ -3,18 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config.js');
-require('./routes.js')(app);
+const cors=require("cors");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 //Enabling CORS for all HTTP methods
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors())
+
+require('./routes.js')(app);
 
 mongoose.Promise = global.Promise;
 
